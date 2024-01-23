32014f1e12c8b551e7ca7effb242157a6dada2c2
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-01-23 10:48:28 -0300

    instalei o express e utilizei o Express.Multer.File para pegar o arquivo

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller('/api/v1')
 export class FileController {
   @Post('/file-upload')
-  async fileUpload() {
+  @UseInterceptors(FileInterceptor('file'))
+  async fileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
     return { message: 'Upload de arquivo realizado com sucesso!' };
   }
 }
