1412445cc6924c335244b9f1b8714bce885b8a37
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-01-23 15:38:04 -0300

    adicionei o csv parser no file controller

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -9,6 +9,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express';
 import { FileRepository } from './file.repository';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import * as csvParser from 'csv-parser';
 
 @Controller('/api/v1')
 export class FileController {
@@ -24,8 +25,18 @@ export class FileController {
   async fileUpload(@UploadedFile() file: Express.Multer.File) {
     const bufferString = file.buffer.toString('utf-8');
 
-    this.fileRepository.saveDataWorksheet(bufferString);
-    return { bufferString };
+    const results = [];
+    const parser = csvParser();
+    parser.write(bufferString);
+    parser.end();
+    parser.on('data', (data) => {
+      results.push(data);
+    });
+    await new Promise((resolve) => parser.on('end', resolve));
+
+    await this.fileRepository.saveDataWorksheet(results);
+
+    return { results };
   }
 
   @Get('/list-data')
