e3ccafcafcdd7f3d6f5b78dc041e30a4fcd4d922
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-01-24 16:56:06 -0300

    enriqueci o arquivo `file.entity.ts` e criei a tabela no postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { FileModule } from './file/file.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [FileModule],
+  imports: [
+    FileModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- src/file/file.entity.ts
+++ src/file/file.entity.ts
@@ -1,12 +1,50 @@
+import {
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'files' })
 export class FileEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'quantidade_cobrancas', length: 20, nullable: false })
   quantidade_cobrancas: string;
+
+  @Column({ name: 'cobrada_a_cada_x_dias', length: 20, nullable: false })
   cobrada_a_cada_x_dias: string;
+
+  @Column({ name: 'data_inicio', length: 40, nullable: false })
   data_inicio: string;
+
+  @Column({ name: 'status', length: 50, nullable: false })
   status: string;
+
+  @Column({ name: 'data_status', length: 40, nullable: false })
   data_status: string;
+
+  @Column({ name: 'data_cancelamento', length: 40 })
   data_cancelamento: string;
+
+  @Column({ name: 'valor', length: 50, nullable: false })
   valor: string;
+
+  @Column({ name: 'proximo_ciclo', length: 40, nullable: false })
   proximo_ciclo: string;
+
+  @Column({ name: 'id_assinante', length: 20, nullable: false })
   id_assinante: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
